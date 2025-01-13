9eccf186d6783478e4e96accbf5f24a62159a12c
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-13 12:49:07 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,24 @@
+class logical
+{
+public static void main(String args[])
+{
+int a=10,b=20,c=30;
+
+if(a<b && a<c)
+{
+System.out.println("a is small");
+}
+else
+{
+System.out.println("a is larger");
+}
+
+if(c>b || a<b)
+{
+System.out.println("c is greater than b or a is less than b");
+}
+else
+{
+}
+}
+}

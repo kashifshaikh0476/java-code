5b88b9bec55da0180b60fd1d5c67c13d7dd3976f
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-13 12:49:59 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,24 @@
+class relational
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+if(a==b)
+{
+System.out.println("a is equal to b");
+}
+else if(a<b)
+{
+System.out.println("a is less than b");
+}
+else if(a>b)
+{
+System.out.println("a is greater than b");
+}
+else 
+{
+System.out.println("a is not equal to b");
+}
+}
+}
+

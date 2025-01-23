af761846f2640c7dfa32e61112a28e853838e58c
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-23 09:50:39 +0530

    Add files via upload

---
+++ defaultcon.java
@@ -0,0 +1,11 @@
+class defaultcon
+{
+    defaultcon()
+    {
+        System.out.println("default cons...!");
+    }
+    public static void main(String args[])
+    {
+        defaultcon d =new defaultcon();
+    }
+}

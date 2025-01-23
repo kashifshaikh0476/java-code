1780ac8e5a028f2583b6575328bf1b3a8ef79f8d
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-23 23:28:36 +0530

    Add files via upload

---
+++ dcons.java
@@ -0,0 +1,13 @@
+class dcons
+{
+    int a=10;
+    int b=20;
+    dcons()
+    {
+        System.out.println("a="+a+ " b="+b);
+    }
+    public static void main(String args[])
+    {
+        dcons d=new dcons();
+    }
+}

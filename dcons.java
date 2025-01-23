ff5018304ac662a1d56b13fc39029df51bc4ee8b
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-23 23:31:15 +0530

    Add files via upload

---
+++ dcons.java
@@ -0,0 +1,14 @@
+//program of parameterized constructor:-
+class dcons
+{
+    int a=10;
+    int b=20;
+    dcons()
+    {
+        System.out.println("a="+a+ " b="+b);
+    }
+    public static void main(String args[])
+    {
+        dcons d=new dcons();
+    }
+}

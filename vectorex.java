7c33e8ec7df0d249572507bb9c89e14236c2bd97
kashif shaikh <shaikhkashif8684@gmail.com>
2025-01-23 10:23:53 +0530

    Add files via upload

---
+++ vectorex.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vectorex {
+    public static void main(String[] args) {
+        Vector<String> Vec = new Vector<String>();
+        
+        Vec.add("Apple");
+        Vec.add("Banana");
+        Vec.add("mango");
+        Vec.add("cherry");
+        System.out.println("fruits are: " + Vec);
+    }
+}
